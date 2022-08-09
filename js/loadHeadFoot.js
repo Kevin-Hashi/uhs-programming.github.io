b034a9f7970e7743e6fd48feab463dfbf3ac39ca
Kevin-Hashi <hashiguchisuteaka@gmail.com>
2022-08-09 21:09:34 +0900

    Update: loadHeadFoot.js htmlのパス

--- js/loadHeadFoot.js
+++ js/loadHeadFoot.js
@@ -6,22 +6,22 @@ document.addEventListener('DOMContentLoaded', function () {
     const DParser = new DOMParser();
     const XSerializer = new XMLSerializer();
     link.insertAdjacentHTML('beforebegin', `<link rel="stylesheet" href="${path}style/bootstrap-reboot.css" />`)
-    fetch('../header.html')
+    fetch(path + '/header.html')
         .then((res) => res.text())
         .then((text) => {
             return DParser.parseFromString(text, 'text/html');
         })
         .then((dom) => {
             body.insertAdjacentHTML('afterbegin', XSerializer.serializeToString(dom.getElementsByTagName('header').item(0)));
-            link.insertAdjacentHTML('beforebegin', XSerializer.serializeToString(dom.getElementsByTagName('head').item(0)).replace(/<\/?head>/, '').replace("./",path));
+            link.insertAdjacentHTML('beforebegin', XSerializer.serializeToString(dom.getElementsByTagName('head').item(0)).replace(/<\/?head>/, '').replace("./", path));
         });
-    fetch('../footer.html')
+    fetch(path + '/footer.html')
         .then((res) => res.text())
         .then((text) => {
             return DParser.parseFromString(text, 'text/html');
         })
         .then((dom) => {
             body.insertAdjacentHTML('beforeend', XSerializer.serializeToString(dom.getElementsByTagName('footer').item(0)));
-            link.insertAdjacentHTML('beforebegin', XSerializer.serializeToString(dom.getElementsByTagName('head').item(0)).replace(/<\/?head>/, '').replace("./",path));
+            link.insertAdjacentHTML('beforebegin', XSerializer.serializeToString(dom.getElementsByTagName('head').item(0)).replace(/<\/?head>/, '').replace("./", path));
         });
 })
